cc45464b7d2ff7a11efa361740e1da59b779fb5e
Philippe Dionne <dionne.phil@gmail.com>
2013-11-06 18:30:22 -0500

    Add bootstrap using bower

--- config.rb
+++ config.rb
@@ -28,6 +28,8 @@
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+sprockets.append_path File.join("#{root}", "bower_components")
+
 activate :directory_indexes
 
 page "/projects/*", layout: :project
