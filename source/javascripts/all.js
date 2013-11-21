e07eaeb9eda60eec96f9fdd583a5650a21e910e3
Philippe Dionne <dionne.phil@gmail.com>
2013-11-20 22:33:07 -0500

    Add masthead background image parallax effect

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,11 @@
 //= require_tree .
+
+$(window).on('scroll', function(e) {
+  parallax();
+});
+
+function parallax() {
+  var scrolled = $(window).scrollTop();
+
+  $('#masthead').css('background-position', ['0px', -(scrolled * 0.3) + 'px'].join(' '));
+}
