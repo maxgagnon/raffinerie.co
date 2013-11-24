f6fc436c0bd58bbb9262777840351451c4457836
Philippe Dionne <dionne.phil@gmail.com>
2013-11-24 18:47:19 -0500

    Handle navigation menu transition

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,10 +2,19 @@
 
 $(window).on('scroll', function(e) {
   parallax();
+  navigation();
 });
 
 function parallax() {
   var scrolled = $(window).scrollTop();
 
   $('#masthead').css('background-position', ['0px', -(scrolled * 0.3) + 'px'].join(' '));
+}
+
+
+function navigation() {
+  var scrolled = $(window).scrollTop();
+  var breakpoint = $('#masthead').outerHeight();
+
+  $('.navbar-transparent').toggleClass('navbar-light', (scrolled > breakpoint));
 }
