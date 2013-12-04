af49fc4d6b5df6fb320aa688608f6293c209918c
Philippe Dionne <dionne.phil@gmail.com>
2013-12-04 11:00:32 -0500

    Show reel in lightbox

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,7 +2,21 @@
 
 $(document).ready(function(){
   $("#project-media .media").fitVids();
+  $(".vimeo").colorbox({ iframe: true, innerWidth: 800, innerHeight: 450 });
 });
 
 
+// Resize Colorbox when resizing window or changing mobile device orientation
+$(window).resize(resizeColorBox);
+window.addEventListener("orientationchange", resizeColorBox, false);
+
+var resizeTimer;
+function resizeColorBox()
+{
+  if (resizeTimer) clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function() {
+    if ($('#cboxOverlay').is(':visible')) {
+      $.colorbox.resize({ width: '90%', height: '60%' });
+    }
+  }, 300);
 }
